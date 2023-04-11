2a033a4427ee7922a7af4b478cebee6ed1df09b0
Vedant Chainani <vedantchainani1084@gmail.com>
2023-04-11 18:35:30 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./layouts/**/*.{js,ts,jsx,tsx}',
+		'./sections/**/*.{js,ts,jsx,tsx}',
+		'./context/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				rubik: ['Rubik', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
