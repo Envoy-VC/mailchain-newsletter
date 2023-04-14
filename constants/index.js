1addaa361c3fbd23b01254b672905c0c95436fdd
Vedant Chainani <vedantchainani1084@gmail.com>
2023-04-14 19:46:17 +0530

    Add Constants and Assets

---
+++ constants/index.js
@@ -0,0 +1,76 @@
+export const message = `
+<div style="max-width: 600px; font-family: Arial, sans-serif; padding: 20px">
+	<div
+		style="
+			max-width: 600px;
+			margin: 0 auto;
+			background-color: #ffffff;
+			padding: 20px;
+			border-radius: 5px;
+			box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+		"
+	>
+		<h1
+			style="
+				background: linear-gradient(#30cfd0, #c43ad6);
+				-webkit-background-clip: text;
+				-webkit-text-fill-color: transparent;
+				font-size: 36px;
+				margin-top: 16px;
+				padding-bottom: 16px;
+                font-weight: 800;
+			"
+		>
+			GM!
+		</h1>
+		<p style="color: #333333; line-height: 1.5; padding-bottom: 8px">
+			Even if it’s not morning where you are, it’s morning in Web3! 🌞
+		</p>
+		<p style="color: #333333; line-height: 1.5; padding-bottom: 8px">
+			Thank you for being a part of WAGMI Weekly - Your Web3 Fun-dose!
+		</p>
+		<p style="color: #333333; line-height: 1.5; padding-bottom: 8px">
+			Get ready for another exciting edition of our newsletter, packed with fun,
+			laughter, and expert analysis of all things Web3!
+		</p>
+		<p style="color: #333333; line-height: 1.5; padding-bottom: 8px">
+			As always, we'll be serving up your weekly dose of Web3 news and
+			entertainment, with a touch of coffee ☕️
+		</p>
+		<div style="display: inline-block; margin-top: 10px; margin-bottom: 10px">
+			<img
+				src="https://i.ibb.co/KNF9zhg/johnny-test-bling-bling-boy.gif"
+				alt="Coffee"
+				style="max-width: 600px; height: auto"
+			/>
+		</div>
+		<p style="color: #999999; font-style: italic">
+			"Web3 without coffee is like a blockchain without blocks!"
+		</p>
+		<div style="display: block; margin-top: 20px; text-align: center">
+			<a
+				href="https://example.com"
+				target="_blank"
+				style="
+					display: inline-block;
+					padding: 10px 20px;
+					background-color: #0066cc;
+					color: #ffffff;
+					text-decoration: none;
+					border-radius: 5px;
+				"
+				>Read Now!</a
+			>
+		</div>
+		<div class="footer">
+			<p style="color: #333333; line-height: 1.5; padding-bottom: 8px">
+				WAGMI Weekly - Your Web3 Fun-dose!
+			</p>
+			<p style="color: #333333; line-height: 1.5; padding-bottom: 8px">
+				Questions or comments? Contact us
+				<a href="mailto:info@example.com">here</a>.
+			</p>
+		</div>
+	</div>
+</div>
+`;
